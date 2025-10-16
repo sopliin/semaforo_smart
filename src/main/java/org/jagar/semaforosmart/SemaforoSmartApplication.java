78310f165e789673c98749477474740c40886352
Rodrigo R. <rrojass@pucp.edu.pe>
2025-10-16 12:45:42 -0500

    Creación del proyecto

---
+++ src/main/java/org/jagar/semaforosmart/SemaforoSmartApplication.java
@@ -0,0 +1,13 @@
+package org.jagar.semaforosmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemaforoSmartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemaforoSmartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jagar/semaforosmart/SemaforoSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jagar.semaforosmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemaforoSmartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
